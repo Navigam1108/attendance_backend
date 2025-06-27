9267da221d9f2c4bf51074e1abbc534dbeb74cde
Navigam <pratham.iiita@gmail.com>
2025-06-27 18:55:22 +0530

    change for seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcryptjs'); // For password hashing
 const pool = process.env.DATABASE_URL
   ? new Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+      ssl: { rejectUnauthorized: false }, // Always require SSL for cloud DB
     })
   : new Pool({
       user: process.env.PGUSER,
